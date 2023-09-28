a7095526c41bece6494c344f9528f8aacec33ade
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-09-28 01:42:42 +0000

    add blink.cpp

---
+++ blink.cpp
@@ -0,0 +1,21 @@
+/*
+C++ code
+Created by: Youngwook Go
+Created on: Sep 2023
+Blinks a LED
+*/
+
+int pin5 = 5;
+
+void setup()
+{
+  pinMode(pin5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(pin5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin5, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
